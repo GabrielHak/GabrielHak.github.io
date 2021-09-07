94c0219fc5b29d4a6fb78a68114016ba8431973a
Roberto Hak <robertohak270997@gmail.com>
2021-09-07 20:49:49 -0300

    webpage

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const btnN = document.querySelector(".btnN");
+const btnP = document.querySelector(".btnP");
+
+const imgs = document.querySelectorAll(".img");
+const imgsA = ["img/html.png", "img/css.png", "img/javascript.png", "img/java.png"];
+
+let cycle = 0;
+
+load(cycle);
+
+btnN.addEventListener("click", ()=>{
+    if(cycle < 4) cycle++;
+    else cycle = 1;
+    load(cycle);
+});
+
+btnP.addEventListener("click", ()=>{
+    if(cycle > 0) cycle--;
+    else cycle = 3;
+    load(cycle);
+});
+
+function load(n){
+    for(let i = 0; i < 3; i++){
+        if(n < imgsA.length){
+            imgs[i].src = imgsA[n];
+        }else{
+            n = 0;
+            imgs[i].src = imgsA[n];
+        }
+        n++;
+    }
+}
